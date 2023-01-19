c8ed4f547bd99144ac6eabf070a0c35146118acf
Anthony Clint Pastorello <dev.anthonyclint@gmail.com>
2023-01-19 16:05:45 -0300

    fix: alterando o MenuLink pelo NavLink do react-router.

--- ola-mundo/src/components/menuLink/index.jsx
+++ ola-mundo/src/components/menuLink/index.jsx
@@ -1,39 +1,18 @@
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import styles from './menuLink.module.css';
 
 export default function MenuLink({ children, to }) {
-    const location = useLocation(); //hook do router-dom
 
     return(
-        <Link 
-            className={ `
-            ${styles.link} 
-            ${location.pathname === to ? styles.linkHighlighted : ''}
-            ` } to={ to }>
+        <NavLink 
+            className={ ({ isActive }) => `
+                ${styles.link} 
+                ${ isActive ? styles.linkHighlighted : ''}
+            `} 
+            to={ to }
+            end >
                 { children }
-        </Link>
+        </NavLink>
     );
 }
-
-/*
-import { NavLink } from 'react-router-dom';
-import styles from './MenuLink.module.css';
-
-export default function MenuLink({ children, to }) {
-
-    return (
-        <NavLink
-            className={({ isActive }) => `
-                ${styles.link}
-                ${isActive ? styles.linkDestacado : ""}
-            `}
-            to={to}
-            end
-        >
-            {children}
-        </NavLink>
-    )
-}
-
-*/
