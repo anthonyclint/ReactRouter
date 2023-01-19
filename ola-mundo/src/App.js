ed7d7df2a8b14767387af02ed7e79036f1f346f3
Anthony Clint Pastorello <dev.anthonyclint@gmail.com>
2023-01-19 14:41:34 -0300

    feat: instalando react router e criando as primeiras rotas.

--- ola-mundo/src/App.js
+++ ola-mundo/src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home";
+import About from "./pages/about";
 
 function App() {
   return (
-    <h1>Olá Mundo!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='*' element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
